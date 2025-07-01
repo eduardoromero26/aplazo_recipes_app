994cb48e8172bd9978c3abaa3c3cd05739ea2f89
Edu <edu.roc26@gmail.com>
2025-07-01 04:58:03 -0600

    feat: add env

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:aplazo_recipes_app/routes/router_generator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: "assets/env/.env.dev");
   runApp(const MyApp());
 }
 

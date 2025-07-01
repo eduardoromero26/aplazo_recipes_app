394a0fde321953e9183ea2621784a9f7eadc3e03
Edu <edu.roc26@gmail.com>
2025-07-01 05:09:41 -0600

    feat: create base api

---
+++ lib/data/services/base_api.dart
@@ -0,0 +1,14 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class BaseApi {
+  final Dio dio = Dio();
+  Future getFromApi(String endpoint) async {
+    try {
+      final response = await dio.get('${dotenv.env['API_URL']}$endpoint');
+      return response;
+    } on DioException catch (e) {
+      return e;
+    }
+  }
+}

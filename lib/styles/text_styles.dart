6969b61fd349d599941eb25b9ceae829ad5f27ee
Edu <edu.roc26@gmail.com>
2025-07-01 04:45:45 -0600

    feat: create assets and style

---
+++ lib/styles/text_styles.dart
@@ -0,0 +1,32 @@
+import 'package:aplazo_recipes_app/styles/colors_theme.dart';
+import 'package:flutter/material.dart';
+
+class TypographyTheme {
+  static const fontSemi24Px = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    fontFamily: 'Rubik',
+    color: ColorsTheme.headingColor,
+  );
+  static const fontSemi20Px = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    fontFamily: 'Rubik',
+    color: ColorsTheme.headingColor,
+  );
+  static const fontMedium20Px = TextStyle(
+    fontSize: 20,
+    fontFamily: 'Rubik',
+    color: ColorsTheme.headingColor,
+  );
+  static const fontRegular16Px = TextStyle(
+    fontSize: 16,
+    fontFamily: 'Rubik',
+    color: ColorsTheme.textColor,
+  );
+  static const fontLight14Px = TextStyle(
+    fontSize: 14,
+    fontFamily: 'Rubik',
+    color: ColorsTheme.textColor,
+  );
+}
